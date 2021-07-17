722fc44ce7e8200f5c9077d5e97d94f527d1474d
deniolp <deniolp@gmail.com>
2021-07-17 15:20:58 +0300

    Creating app listening ws through ports in elm

---
+++ src/WebSocket.elm
@@ -0,0 +1,112 @@
+port module WebSocket exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Events exposing (..)
+import LeaderBoard exposing (Model, Msg)
+
+
+
+-- main
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- ports
+
+
+port sendMessage : String -> Cmd msg
+
+
+port messageReceiver : (String -> msg) -> Sub msg
+
+
+
+-- model
+
+
+type alias Model =
+    { streamTime : Bool
+    , time : String
+    }
+
+
+initModel : Model
+initModel =
+    { streamTime = False
+    , time = ""
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initModel, Cmd.none )
+
+
+
+-- update
+
+
+type Msg
+    = ToggleStreaming
+    | Time String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ToggleStreaming ->
+            let
+                newModel =
+                    { model | streamTime = not model.streamTime }
+
+                message =
+                    if newModel.streamTime then
+                        "start"
+
+                    else
+                        "stop"
+            in
+            ( newModel, sendMessage message )
+
+        Time time ->
+            ( { model | time = time }, Cmd.none )
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    let
+        toggleLabel =
+            if model.streamTime then
+                "Stop"
+
+            else
+                "Start"
+    in
+    div []
+        [ button [ onClick ToggleStreaming ] [ text toggleLabel ]
+        , br [] []
+        , text model.time
+        ]
+
+
+
+-- subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    messageReceiver Time
